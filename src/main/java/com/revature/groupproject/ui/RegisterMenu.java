e44f70696db7e282fdbdbc650772656d00d29ee4
victo <victoriatrotter95@gmail.com>
2022-06-02 19:25:40 -0400

    Working on it

--- src/main/java/com/revature/groupproject/ui/RegisterMenu.java
+++ src/main/java/com/revature/groupproject/ui/RegisterMenu.java
@@ -1,8 +1,39 @@
 package com.revature.groupproject.ui;
 
+import com.revature.groupproject.models.Courses;
+import com.revature.groupproject.services.CoursesServices;
+import jdk.internal.jrtfs.JrtPath;
+
+import java.util.Scanner;
+
 public class RegisterMenu implements IMenu{
     @Override
     public void start() {
+        Scanner scanner=new Scanner(System.in);
+
+        exit:{
+            while(true){
+                System.out.println("\nWelcome to the Register menu:");
+                System.out.println("[1]Register for Classes");
+                System.out.println("[x]Sign out");
+
+                System.out.println("\nEnter:");
+
+                switch(scanner.nextLine()){
+                    case "1":
+
+                        break ;
+                    case"x":
+                        break exit;
+                    default:
+                        System.out.println("\nInvalid input");
+                        break;
+                }
+            }
+        }
 
     }
+
+
+
 }
