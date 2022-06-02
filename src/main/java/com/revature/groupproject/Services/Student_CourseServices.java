053619881878780a375b8e44240eb18a11ea1d41
igeet <105670057+igeet@users.noreply.github.com>
2022-06-02 16:32:41 -0500

    Update Student_CourseServices.java

--- src/main/java/com/revature/groupproject/Services/Student_CourseServices.java
+++ src/main/java/com/revature/groupproject/Services/Student_CourseServices.java
@@ -1,1 +1,14 @@
+package com.revature.groupproject.services;
+
+import com.revature.groupproject.daos.Student_CoursesDAO;
+import com.revature.groupproject.models.student_course;
+
+public class Students_CoursesServices {
+
+    private final Student_CoursesDAO student_coursesDAO;
+
+    public students_coursesServices(Student_CoursesDAO student_coursesDAO){
+        this.student_coursesDAO = student_coursesDAO;
+    }
+}
 
