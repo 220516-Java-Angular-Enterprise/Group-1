658293e82ca4a896410ebd46ec9f30178ef65bcb
igeet <105670057+igeet@users.noreply.github.com>
2022-06-02 16:23:24 -0500

    Update StudentsServices.java

--- src/main/java/com/revature/groupproject/Services/StudentsServices.java
+++ src/main/java/com/revature/groupproject/Services/StudentsServices.java
@@ -1,1 +1,40 @@
+package com.revature.groupproject.services;
+
+import com.revature.groupproject.models.Students;
+import com.revature.groupproject.daos.StudentsDAO;
+import com.revature.groupproject.util.custom_exception.InvalidStudentsException;
+
+import java.util.List;
+
+public class StudentsServices {
+
+    private final StudentsDAO studentsDAO;
+
+    public StudentsServices(StudentsDAO studentsDAO){
+        this.studentsDAO = studentsDAO;
+    }
+
+    public boolean login(String username, String password){
+
+        List<Students> students = StudentsDAO.getALL();
+
+        for(Students s : students){
+
+            if(s.getUsername() .equals(username) && s.getPassword() .equals(password))
+            { return true;}
+
+        }
+        return false;
+    }
+
+    private Students isValidCredentials(Students students){
+        if(students.getUsername() == null && students.getPassword() == null)
+            throw new InvalidStudentsException("Username and Password are incorrect");
+
+        else if (students.getUsername() == null) throw new InvalidStudentsException("Incorrect username");
+        else if (students.getPassword() == null) throw new InvalidStudentsException("Incorrect password");
+
+        return students;
+    }
+}
 
