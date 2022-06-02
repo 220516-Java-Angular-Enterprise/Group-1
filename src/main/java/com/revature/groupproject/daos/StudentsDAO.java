62cf94978422c9c60c65e4170438fe832d5c7f74
Matthew Berkheimer <MatthewBerkheimerWork@gmail.com>
2022-06-02 14:27:12 -0700

    Fixed the DAO's tyvm victoria

--- src/main/java/com/revature/groupproject/daos/StudentsDAO.java
+++ src/main/java/com/revature/groupproject/daos/StudentsDAO.java
@@ -5,31 +5,39 @@ import com.revature.groupproject.util.database.DatabaseConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-<<<<<<< HEAD
-=======
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
->>>>>>> 5866a13ce5df2e05c07c96c6a497389538ee9f9b
 import java.util.List;
 
-public class StudentsDAO implements CRUDDao{
-    Connection con= DatabaseConnection.getCon();
-    @Override
-    public void save(Object obj) {
-<<<<<<< HEAD
-        try{
-            PreparedStatement ps = con.prepareStatement("INSERT INTO students(id,username,password,name) VALUES(?,?,?,?)");
-            ps.setString(1,obj.getId());
-            ps.setString(2,obj.getUsername());
-            ps.setString(3,obj.getPassword);
-            ps.setString(4,obj.getName);
-=======
+public class StudentsDAO implements CRUDDao<Students> {
+    Connection con = DatabaseConnection.getCon();
 
->>>>>>> 5866a13ce5df2e05c07c96c6a497389538ee9f9b
-    }
     @Override
-    public void update(Object obj) {
+    public void save(Students obj) {
+    }
+
+    @Override
+    public List<Students> getAll() {
+        List<Students> studentsList = new ArrayList<>();
+        try {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM students");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Students students = new Students(rs.getString("id"), rs.getString("username"), rs.getString("password"), rs.getString("name"));
+                studentsList.add(students);
+            }
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        }
+        return studentsList;
+    }
+
+
+    @Override
+    public void update(Students obj) {
 
     }
 
@@ -39,25 +47,7 @@ public class StudentsDAO implements CRUDDao{
     }
 
     @Override
-    public List getAll() {
-        List<Students> studentsList = new ArrayList<>();
-        try{
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM students");
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()){
-                Students students = new Students(rs.getString("id"), rs.getString("username"), rs.getString("password"), rs.getString("name"));
-                studentsList.add(students);
-            }
-        }catch (SQLException e){
-            System.out.println("SQLException: " + e.getMessage());
-            System.out.println("SQLState: " + e.getSQLState());
-            System.out.println("VendorError: " + e.getErrorCode());
-        }
-        return studentsList;
-    }
-
-    @Override
-    public List getById(String id) {
+    public List<Students> getById(String id) {
         return null;
     }
 }
