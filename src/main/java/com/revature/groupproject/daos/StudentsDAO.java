335c55a493acf4fcea37f9eb1c68d6f3df668147
Matthew Berkheimer <MatthewBerkheimerWork@gmail.com>
2022-06-02 13:33:16 -0700

    StudentDAO start

--- src/main/java/com/revature/groupproject/daos/StudentsDAO.java
+++ src/main/java/com/revature/groupproject/daos/StudentsDAO.java
@@ -1,15 +1,20 @@
 package com.revature.groupproject.daos;
 
+import com.revature.groupproject.models.Students;
 import com.revature.groupproject.util.database.DatabaseConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentsDAO implements CRUDDao{
     Connection con= DatabaseConnection.getCon();
     @Override
     public void save(Object obj) {
-        try{
+
     }
     @Override
     public void update(Object obj) {
@@ -23,7 +28,20 @@ public class StudentsDAO implements CRUDDao{
 
     @Override
     public List getAll() {
-        return null;
+        List<Students> studentsList = new ArrayList<>();
+        try{
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM students");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                Students students = new Students(rs.getString("id"), rs.getString("username"), rs.getString("password"), rs.getString("name"));
+                studentsList.add(students);
+            }
+        }catch (SQLException e){
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        }
+        return studentsList;
     }
 
     @Override
