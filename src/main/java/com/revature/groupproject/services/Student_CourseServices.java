75118fc7d1e3bf1e040218a6ec14487831b45b64
igeet <105670057+igeet@users.noreply.github.com>
2022-06-02 20:34:21 -0500

    Update Student_CourseServices.java

--- src/main/java/com/revature/groupproject/services/Student_CourseServices.java
+++ src/main/java/com/revature/groupproject/services/Student_CourseServices.java
@@ -1,6 +1,13 @@
 package com.revature.groupproject.services;
 
 import com.revature.groupproject.daos.Student_CourseDAO;
+import com.revature.groupproject.models.Student_Course;
+import com.revature.groupproject.util.custom_exception.InvalidSQLException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 
 public class Student_CourseServices {
 
@@ -9,5 +16,24 @@ public class Student_CourseServices {
     public Student_CourseServices(Student_CourseDAO student_coursesDAO){
         this.student_coursesDAO = student_coursesDAO;
     }
-}
 
+    public void register(Student_Course student_course){
+        student_coursesDAO.save(student_course);
+
+    }
+
+    public List<Student_Course>getAllStudent_Course(String id){
+        List<Student_Course> otherList = student_coursesDAO.getAll();
+        List<Student_Course> student_course = otherList.stream().filter(studentCourse -> studentCourse.getStudentID().equals(id)).collect(Collectors.toList());
+        return student_course;
+    }
+
+    public void deletestudentCourse(Student_Course student_course){
+            String studentid = student_course.getStudentID();
+            String courseid = student_course.getCourseID();
+            student_coursesDAO.delete(studentid, courseid);
+
+        }
+
+
+}
